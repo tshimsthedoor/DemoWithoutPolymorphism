7fd888422aa46850a1001f78c4cc32ad5561a2db
tresor <tshims79@gmail.com>
2023-06-18 18:51:04 +0200

    Add project files.

---
+++ DemoWithoutPolymorphism/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DemoWithoutPolymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("******Sounds of the different animals.******");
+            Tiger tiger = new Tiger();
+            tiger.Sound();
+
+            Dog dog = new Dog();
+            dog.Sound();
+
+            Monkey monkey = new Monkey();
+            monkey.Sound();
+            Console.ReadLine();
+        }
+    }
+}
